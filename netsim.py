4f3ffaf27e834409257781ba846c4e3caa88ce2b
aandujar2641 <aandujar@udel.edu>
2023-11-29 13:32:43 -0500

    Added imports and mtx files to netsim

--- netsim.py
+++ netsim.py
@@ -3,3 +3,9 @@ Final Project - Virus Spread Containment Simulation
 CISC 489-012 
 Authors: Anthony Andujar, Tabetha Chubb
 '''
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+network = nx.read_edgelist('networkfile.mtx', create_using=nx.Graph(), nodetype=int)
+infectednodes = np.loadtxt('infectedfile.mtx', dtype=int)
