2236d941fc71a4bb8a382b50c28165e13d54d263
Tabetha <tabc@udel.edu>
2023-12-06 21:20:28 -0500

    Graph Sim code

--- netsim.py
+++ netsim.py
@@ -1,70 +1,96 @@
-'''
-Final Project - Virus Spread Containment Simulation
-CISC 489-012 
-Authors: Anthony Andujar, Tabetha Chubb
-'''
-import networkx as nx
-import matplotlib.pyplot as plt
-import numpy as np
 import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
-# Reads the network file into a graph variable
-def readNetworkFile(network):
-    graph = nx.Graph()
-    with open(network, 'r') as file:
-        for line in file:
-            source, target, probability = map(float, line.split())
-            graph.add_edge(int(source), int(target), probability=probability)
-    return graph
 
-# Reads the infected nodes file into a variable
-def readInfectedFile(infectedFile):
-    with open(infectedFile, 'r') as file:
-        infectedNodes = {int(line.strip()) for line in file}
-    return infectedNodes
+def read_network_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        edges = [tuple(map(float, line.split(" "))) for line in lines]
+    return edges
 
-def virus_spread_iteration(graph, infected_nodes, L):
-    # Implement virus spread iteration
-    pass
+def read_infected_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        infected_nodes = [float(line) for line in lines]
+    return infected_nodes
 
-def immunization_iteration(graph, B, M):
-    # Implement immunization iteration
-    pass
+def simulate_virus_spread(graph, infected_nodes, probability_of_spread, L):
+    new_infected = set()
 
-def main(network_file, infected_file, T, L, M, B):
-    # Read input files
-    network = read_network_file(network_file)
-    initial_infected_nodes = read_infected_file(infected_file)
+    for node in infected_nodes:
+        neighbors = set(graph.neighbors(node))
+        susceptible_neighbors = neighbors - infected_nodes
 
-    # Initialize graph and infected nodes
-    graph = nx.Graph(network)
-    infected_nodes = set(initial_infected_nodes)
+        for neighbor in susceptible_neighbors:
+            if random.random() < probability_of_spread:
+                new_infected.add(neighbor)
 
-    # Simulation loop
-    for iteration in range(1, T + 1):
-        if iteration % 2 == 1:
-            # Virus Spread Iteration
-            infected_nodes = virus_spread_iteration(graph, infected_nodes, L)
-        else:
-            # Immunization Iteration
-            immunization_iteration(graph, B, M)
+    return new_infected, infected_nodes
 
-        # Output current status
-        #print(f"{['VSI', 'II'][iteration % 2 - 1]}: {len(infected_nodes)} {len(graph.nodes) - len(infected_nodes)} {len(immunized_nodes)}")
+def simulate_immunization(graph, immunized_nodes, B, M):
+    immunized_nodes = set(random.sample(set(graph.nodes) - immunized_nodes, B))
+    return immunized_nodes, immunized_nodes
 
-    # Generate and save the plot
-    # Implement code for plotting
+def plot_simulation(iterations, infected_counts, susceptible_counts, immunized_counts):
+    plt.plot(iterations, infected_counts, label='Infected Nodes')
+    plt.plot(iterations, susceptible_counts, label='Susceptible Nodes')
+    plt.plot(iterations, immunized_counts, label='Immunized Nodes')
+    plt.xlabel('Iteration')
+    plt.ylabel('Node Count')
+    plt.legend()
+    plt.title('Virus Spread and Immunization Simulation')
+    plt.show()
+
+def main():
+    '''
+    parser = argparse.ArgumentParser(description='Virus Spread and Containment Simulation')
+    parser.add_argument('-networkfile', type=str, default='infectedgraph.edges', help='Path to the network file')
+    parser.add_argument('-infectedfile', type=str, default='infectednodes.txt', help='Path to the infected nodes file')
+    #parser.add_argument('-T', type=int, required=True, help='Total number of iteration pairs')
+    #parser.add_argument('-L', type=int, required=True, help='Duration of infection for each node')
+    #parser.add_argument('-M', type=int, required=True, help='Duration of immunization for each node')
+    #parser.add_argument('-B', type=int, required=True, help='Number of antivirus shots for immunization')
+    args = parser.parse_args()
+'''
+    # Read network and infected node files
+    edges = read_network_file("infectedgraph.edges")
+    #args.networkfile
+    infected_nodes = read_infected_file("infectednodes.txt")
+    #args.infectedfile
+
+    # Create a graph from the network file
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    # Simulation parameters
+    probability_of_spread = 0.1
+    iterations = []
+    infected_counts = []
+    susceptible_counts = []
+    immunized_counts = []
+
+    #(1, args.T + 1)
+    for iteration in range(1, 500 + 1):
+        # Virus Spread Iteration
+        new_infected, infected_nodes = simulate_virus_spread(G, infected_nodes, probability_of_spread, 5)
+        #args.L
+        # Immunization Iteration
+        immunized_nodes, immunized_nodes = simulate_immunization(G, immunized_nodes, 50, 10)
+        #args.B, args.M
+        # Update counts
+        iterations.append(iteration)
+        infected_counts.append(len(infected_nodes))
+        susceptible_counts.append(len(G.nodes) - len(infected_nodes) - len(immunized_nodes))
+        immunized_counts.append(len(immunized_nodes))
+
+        # Print output for the current iteration
+        print(f'VSI: {len(infected_nodes)} {len(G.nodes) - len(infected_nodes) - len(immunized_nodes)} {len(immunized_nodes)}')
+
+    # Plot the simulation
+    plot_simulation(iterations, infected_counts, susceptible_counts, immunized_counts)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Virus Spread Containment Simulation")
-    parser.add_argument("-networkfile", type=str, help="Path to the network configuration file")
-    parser.add_argument("-infectedfile", type=str, help="Path to the initial infected nodes file")
-    parser.add_argument("-T", type=int, help="Total number of iteration pairs")
-    parser.add_argument("-L", type=int, help="Infection duration")
-    parser.add_argument("-M", type=int, help="Immunization duration")
-    parser.add_argument("-B", type=int, help="Number of antivirus shots")
-    
-    args = parser.parse_args()
-    main(args.networkfile, args.infectedfile, args.T, args.L, args.M, args.B)
+    main()
+
