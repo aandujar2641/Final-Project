c608a70fa9fab5d21b315ed322dfe6844f518a3e
aandujar2641 <aandujar@udel.edu>
2023-12-08 11:12:45 -0500

    pretty much done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-This is the final project for CISC 489-012, it is a virus spread containment simulation.
-It was created by Anthony Andujar and Tabetha Chubb.
+CISC 489-012
+Final-Project: VSCS
+Authors: Anthony Andujar, Tabetha Chubb
+
+To run this code you need to use the command: 
+python3 netsim.py -networkfile file1 -infectedfile file2 -T num1 -L num2 -M num3 -B num4
+(Changing file1 with the network file, file2 with the infected nodes file, and num# with whatever integer value you want)
--- netsim.py
+++ netsim.py
@@ -8,7 +8,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-
 def read_network_file(filename):
     edges = []
     with open(filename, 'r') as file:
@@ -22,12 +21,12 @@ def read_infected_file(filename):
         infected_nodes = [int(line) for line in file.read().splitlines()]
     return set(infected_nodes)
 
-def simulate_virus_spread(graph, infected_nodes, probability_of_spread, L):
+def simulate_virus_spread(graph, infected_nodes, probability_of_spread, L, recovered_nodes):
     new_infected = set()
 
     for node in infected_nodes:
         neighbors = set(graph.neighbors(node))
-        susceptible_neighbors = neighbors - infected_nodes
+        susceptible_neighbors = neighbors - infected_nodes - recovered_nodes
 
         for neighbor in susceptible_neighbors:
             if random.random() < probability_of_spread:
@@ -35,19 +34,34 @@ def simulate_virus_spread(graph, infected_nodes, probability_of_spread, L):
 
     return new_infected, infected_nodes.union(new_infected)
 
-def simulate_immunization(graph, immunized_nodes, B, M):
-    nodes_list = list(set(graph.nodes) - immunized_nodes)
-    if B >= len(nodes_list):
-        new_immunized_nodes = set(nodes_list)
-    else:
-        new_immunized_nodes = set(random.sample(nodes_list, B))
+def simulate_immunization_community(graph, immunized_nodes, B, M):
+    communities = list(nx.community.greedy_modularity_communities(graph))
     
+    # Exclude already immunized nodes from communities
+    remaining_communities = [community - immunized_nodes for community in communities]
+    
+    immunized_communities = random.sample(remaining_communities, min(len(remaining_communities), B))
+
+    # Flatten the selected communities and take the first M nodes
+    new_immunized_nodes = set(node for community in immunized_communities for node in list(community)[:M])
     return new_immunized_nodes, immunized_nodes.union(new_immunized_nodes)
 
+def simulate_infection_dynamics(infected_nodes, recovered_nodes, M):
+    # Nodes that lost immunization are susceptible to reinfection
+    lost_immunization_nodes = recovered_nodes.intersection(infected_nodes)
+    # Ensure that the number of nodes to be reinfected does not exceed the total infected nodes
+    num_reinfected = min(len(lost_immunization_nodes), M)
+    new_infected = random.sample(list(lost_immunization_nodes), num_reinfected)
+    
+    recovered_nodes.difference_update(new_infected)
+    infected_nodes.update(new_infected)
+    
+    return infected_nodes, recovered_nodes
 
 def plot_simulation(iterations, infected_counts, susceptible_counts, immunized_counts):
+    susceptible_counts_non_negative = [max(0, count) for count in susceptible_counts]
     plt.plot(iterations, infected_counts, label='Infected Nodes')
-    plt.plot(iterations, susceptible_counts, label='Susceptible Nodes')
+    plt.plot(iterations, susceptible_counts_non_negative, label='Susceptible Nodes')
     plt.plot(iterations, immunized_counts, label='Immunized Nodes')
     plt.xlabel('Iteration')
     plt.ylabel('Node Count')
@@ -56,9 +70,19 @@ def plot_simulation(iterations, infected_counts, susceptible_counts, immunized_c
     plt.show()
 
 def main():
+    parser = argparse.ArgumentParser(description='Virus Spread and Immunization Simulation')
+    parser.add_argument('-networkfile', type=str, help='Network file path')
+    parser.add_argument('-infectedfile', type=str, help='Infected file path')
+    parser.add_argument('-T', type=int, default=500, help='Number of iterations')
+    parser.add_argument('-L', type=int, default=5, help='Parameter L')
+    parser.add_argument('-M', type=int, default=10, help='Parameter M')
+    parser.add_argument('-B', type=int, default=50, help='Parameter B')
+
+    args = parser.parse_args()
+
     # Read network and infected node files
-    edges = read_network_file('infectedgraph.edges')
-    infected_nodes = read_infected_file('infectednodes.txt')
+    edges = read_network_file(args.networkfile)
+    infected_nodes = read_infected_file(args.infectedfile)
 
     # Create a graph from the network file
     G = nx.Graph()
@@ -70,24 +94,30 @@ def main():
     infected_counts = []
     susceptible_counts = []
     immunized_counts = []
+    infected_nodes_set = infected_nodes.copy()
+    immunized_nodes = set()
+    recovered_nodes = set()
 
-    for iteration in range(1, 500 + 1):
+    for iteration in range(1, args.T + 1):
         # Virus Spread Iteration
-        new_infected, infected_nodes = simulate_virus_spread(G, infected_nodes, probability_of_spread, 5)
+        new_infected, infected_nodes_set = simulate_virus_spread(G, infected_nodes_set, probability_of_spread, args.L, recovered_nodes)
         # Immunization Iteration
-        immunized_nodes, infected_nodes = simulate_immunization(G, infected_nodes, 50, 10)
+        new_immunized_nodes, immunized_nodes = simulate_immunization_community(G, immunized_nodes, args.B, args.M)
+        # Infection Dynamics
+        infected_nodes_set, recovered_nodes = simulate_infection_dynamics(infected_nodes_set, recovered_nodes, args.M)
+
         # Update counts
         iterations.append(iteration)
-        infected_counts.append(len(infected_nodes))
-        susceptible_counts.append(len(G.nodes) - len(infected_nodes) - len(immunized_nodes))
+        infected_counts.append(len(infected_nodes_set))
+        susceptible_counts.append(len(G.nodes) - len(infected_nodes_set) - len(immunized_nodes))
         immunized_counts.append(len(immunized_nodes))
 
         # Print output for the current iteration
-        print(f'VSI: {len(infected_nodes)} {len(G.nodes) - len(infected_nodes) - len(immunized_nodes)} {len(immunized_nodes)}')
+        print(f'VSI: {len(infected_nodes_set)} {max(len(G.nodes) - len(infected_nodes_set) - len(immunized_nodes), 0)} {len(immunized_nodes)}')
+        print(f'II: {len(new_immunized_nodes)} {max(len(G.nodes) - len(infected_nodes_set) - len(new_immunized_nodes), 0)} {len(immunized_nodes)}')
 
     # Plot the simulation
     plot_simulation(iterations, infected_counts, susceptible_counts, immunized_counts)
 
 if __name__ == "__main__":
     main()
-    
